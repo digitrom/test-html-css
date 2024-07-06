2ac9695a15026cbe798b75598f4dbc7dcfa6b613
digitrom <roma.bobrovski@gmail.com>
2024-07-06 22:39:19 +0200

    commit 1

---
+++ js/1.js
@@ -0,0 +1,12 @@
+// let i = 1;
+//
+// for (i = 1; i < 10; ++i) {
+//     if (i % 2) {
+//         alert(++i)
+//     }
+//     continue
+// }
+
+let i = 0;
+
+while (i < 3) alert( `number ${i++}!` )
